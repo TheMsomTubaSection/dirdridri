8f7eaca3f8f4a2d1deb4abd84d6616aac13c9298
Adrusi <adrian@adrusi.com>
2014-09-28 10:42:48 -0400

    initial read success

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var imagename = "img";
 var mountpoint = process.argv[3];
 console.log(srcroot, mountpoint);
 
-var blocksize = 1024 * 1024; // 1 MiB
+var blocksize = 4096;
 
 var errnoMap = {
   EPERM: 1,
@@ -33,9 +33,13 @@ chmod, create, unlink, rename, mkdir, rmdir = unsupported;
 function getattr(path, callback) {
   console.log("getattr");
   if (path === "/") {
-    callback(0, { size: 9001, mode: 040777, nlink: 1 });
+    callback(0, { size: 4096, mode: 040777, nlink: 1 });
   } else if (path === "/" + imagename) {
-    callback(0, { size: 1337, mode: 010777, nlink: 1 });
+    callback(0, {
+      size: fs.readdirSync(srcroot).length * blocksize,
+      mode: 0100777,
+      nlink: 1
+    });
   } else {
     callback(-errnoMap.ENOENT);
   }
@@ -53,18 +57,56 @@ function open(path, flags, callback) {
   return callback(0);
 }
 
-function read(path, offset, len, buf, _, callback) {
+function read(pth, offset, len, buf, _, callback) {
+  // if (pth !== "/" + imagename) return callback(errnoMap.ENOENT);
+  // var blockid = Math.floor(offset / blocksize);
+  // var bufoffset = 0;
+  // offset %= blocksize;
+  // (function recur(blockid, len, bufoffset, offset) {
+  //   if (len <= 0) {
+  //     console.log("done");
+  //     callback(0);
+  //     return;
+  //   }
+  //   var readlength = Math.min(len, blocksize);
+  //   fs.open(path.join(srcroot, ""+blockid), "r", function (err, fd) {
+  //     if (err) {
+  //       callback(errnoMap.EINVAL);
+  //       return;
+  //     }
+  //     fs.read(fd, buf, bufoffset, readlength, offset, function (err) {
+  //       fs.close(fd, function () {
+  //         console.log(blockid);
+  //         // process.stdout.write(buf.slice(bufoffset, bufoffset + readlength), "utf8");
+  //         if (err) {
+  //           callback(errnoMap.EINVAL);
+  //           return;
+  //         }
+  //         recur(blockid + 1, len - readlength, bufoffset + readlength, 0);
+  //       });
+  //     });
+  //   });
+  // }(blockid, len, bufoffset, offset));
+  if (pth !== "/" + imagename) return callback(errnoMap.ENOENT);
   console.log("read");
-  if (path !== "/" + imagename) return callback(errnoMap.ENOENT);
-  var blockid = Math.floor(offset / blocksize);
-  var bufoffset = 0;
-  offset %= blocksize;
-  while (len > 0) {
-    var readlength = Math.min(len, blocksize);
-    var fd = fs.openSync(path.join(srcroot, blockid), "r");
-    fs.readSync(fd, buf.slice, bufoffset, readlength, offset);
-    len -= readlength;
-  }
+  for (var i = 0; i < len; i++) buf[i] = "@".charCodeAt(0);
+  callback(len);
+  // var blockid = Math.floor(offset / blocksize);
+  // var bufoffset = 0;
+  // offset %= blocksize;
+  // while (len > 0) {
+  //   console.log(blockid);
+  //   var readlen = Math.min(len, blocksize);
+  //   var fd = fs.openSync(path.join(srcroot, ""+blockid), "r");
+  //   fs.readSync(fd, buf, bufoffset, readlen, offset);
+  //   fs.closeSync(fd);
+  //   // process.stdout.write(buf.slice(bufoffset, bufoffset+readlen), "utf8");
+  //   len -= readlen;
+  //   blockid++;
+  //   offset = 0;
+  //   bufoffset += readlen;
+  // }
+  // callback(0);
 }
 
 function write(path, offset, len, buf, _, callback) {
@@ -76,22 +118,25 @@ function write(path, offset, len, buf, _, callback) {
   while (len > 0) {
     var writelen = Math.min(len, blocksize);
     var fd = fs.openSync(path.join(srcroot, blockid), "r+");
-    fs.writeSync(fd, buf.slice, bufoffset, readlength, offset);
+    fs.writeSync(fd, buf, bufoffset, readlength, offset);
     len -= writelen;
+    blockid++;
   }
+  callback(0);
 }
 
-function release() {
+function release(_1, _2, callback) {
+  callback(0);
 }
 
 function init(callback) {
   console.log("init");
-  callback();
+  callback(0);
 }
 
 function destroy(callback) {
   console.log("destroy");
-  callback();
+  callback(0);
 }
 
 function statfs(cb) {
